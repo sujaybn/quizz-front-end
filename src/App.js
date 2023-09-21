f107ca127444cee73b97ad68fb20986a355e317b
sujaybn <89024189+sujaybn@users.noreply.github.com>
2023-09-21 18:55:47 +0530

    Committing the front end quizz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    fetch(
+      'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple&encode=url3986'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the questions from the API response
+        setQuestions(data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleAnswerClick = (selectedOption) => {
+    if (decodeURIComponent(selectedOption) === questions[currentQuestion].correct_answer) {
+      // Increase the score if the answer is correct
+      setScore(score + 1);
+    }
+
+    // Move to the next question
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const renderQuiz = () => {
+    if (currentQuestion >= questions.length) {
+      // Display quiz results when all questions are answered
+      return (
+        <div>
+          <h2>Quiz Completed!</h2>
+          <p>Your Score: {score} / {questions.length}</p>
+        </div>
+      );
+    } else {
+      const currentQues = questions[currentQuestion];
+      return (
+        <div>
+          <h2>Question {currentQuestion + 1}</h2>
+          <p>{decodeURIComponent(currentQues.question)}</p>
+          <ul>
+            {currentQues.incorrect_answers.map((option, index) => (
+              <li key={index} onClick={() => handleAnswerClick(option)}>
+                {decodeURIComponent(option)}
+              </li>
+            ))}
+            <li key="correct" onClick={() => handleAnswerClick(currentQues.correct_answer)}>
+              {decodeURIComponent(currentQues.correct_answer)}
+            </li>
+          </ul>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Quiz App</h1>
+      {questions.length === 0 ? <p>Loading...</p> : renderQuiz()}
     </div>
   );
-}
+};
 
 export default App;
